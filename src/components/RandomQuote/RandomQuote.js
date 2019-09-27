0ea9d25ff2625d2f4d5911d52f69484c5a402728
Simon Liu <cyamonide@gmail.com>
2019-09-27 14:20:50 -0400

    Add quotes

--- src/components/RandomQuote/RandomQuote.js
+++ src/components/RandomQuote/RandomQuote.js
@@ -4,12 +4,13 @@ import "./RandomQuote.css";
 
 const quotes = [
   {
-    text: "Don't use cat gratuitously.",
-    author: "Me"
+    text: "Don't use cat gratuitously."
   },
   {
-    text: "If you're not struggling, you're doing something wrong.",
-    author: "Me"
+    text: "If you're not struggling, you're doing something wrong."
+  },
+  {
+    text: "RIP Power Bridge."
   },
   {
     text: "Racing is life and life is risk.",
@@ -20,8 +21,12 @@ const quotes = [
     author: "Alex Honnold, first to free solo El Capitan"
   },
   {
-    text:
-      "I know it hurts being so far behind this weekend, but let's remember that we do something that we love.",
+    text: (
+      <>
+        I know it hurts being so far behind this weekend, but let's remember
+        that <i>we do something that we love</i>.
+      </>
+    ),
     author: "Sebastian Vettel, 2019 Spanish GP post-race team radio"
   }
 ];
@@ -37,7 +42,7 @@ const RandomQuote = () => {
         <p>{text}</p>
       </div>
       <div>
-        <p>-- {author}</p>
+        <p>{author && `-- ${author}`}</p>
       </div>
     </div>
   );
