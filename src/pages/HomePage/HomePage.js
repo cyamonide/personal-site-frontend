6a66b5e44b5cf60fde4b0079522ea17909a366e9
Simon Liu <cyamonide@gmail.com>
2020-04-04 04:18:06 -0400

    Add personal info to landing

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,15 +1,85 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
 
 import { BusinessCard, SocialCrumbs, RandomQuote } from "../../components";
 import "./HomePage.css";
 
-const HomePage = props => {
+const HomePage = (props) => {
   return (
-    <div className="home-page-wrapper">
-      <BusinessCard />
-      <SocialCrumbs />
-      <RandomQuote />
-    </div>
+    <>
+      <div className="home-page-wrapper">
+        <div>
+          <h1>
+            SIMON LIU <span className="subtle">// cyamonide</span>
+          </h1>
+        </div>
+        <div className="home-page-card">
+          <Grid container direction="row" justify="center" alignItems="center">
+            <Grid item xs={5}>
+              <h2>Contact</h2>
+            </Grid>
+            <Grid item xs={7}>
+              <p>hello@cyamonide.me</p>
+            </Grid>
+          </Grid>
+        </div>
+        <div className="home-page-card">
+          <Grid container direction="row" justify="center" alignItems="center">
+            <Grid item xs={5}>
+              <h2>Education</h2>
+            </Grid>
+            <Grid item xs={7}>
+              <p>
+                Computer Science <span className="subtle">at</span>
+              </p>
+              <p>University of Waterloo</p>
+            </Grid>
+          </Grid>
+        </div>
+        <div className="home-page-card">
+          <Grid container direction="row" justify="center" alignItems="center">
+            <Grid item xs={5}>
+              <h2>Work</h2>
+            </Grid>
+            <Grid item xs={7}>
+              <p>
+                Software Engineering Intern{" "}
+                <span className="subtle">working on</span>
+              </p>
+              <p>
+                Infrastructure <span className="subtle">at</span>
+              </p>
+              <p>
+                Coursera <span className="subtle">Mountain View</span>
+              </p>
+            </Grid>
+          </Grid>
+        </div>
+        <div className="home-page-card">
+          <Grid container direction="row" justify="center" alignItems="center">
+            <Grid item xs={5}>
+              <h2>Interests</h2>
+            </Grid>
+            <Grid item xs={7}>
+              <p>
+                Formula 1 <span className="subtle">fan</span>
+              </p>
+              <p>
+                Mountain bike <span className="subtle">rider</span>
+              </p>
+              <p>
+                Tech <span className="subtle">enthusiast</span>
+              </p>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+      <div className="home-page-wrapper">
+        <BusinessCard />
+        <SocialCrumbs />
+        <RandomQuote />
+      </div>
+    </>
   );
 };
 
